aa5d3b3749f515b2003d26bcce44ec88fcb0a8c8
belov.gr <belov.gr@gmail.com>
2009-10-29 08:45:58 +0000

    chg - Сделан более подробный отчет об ошибках

--- app/Beavers.Encounter.Common/Extensions.cs
+++ app/Beavers.Encounter.Common/Extensions.cs
@@ -1,7 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections;
 using System.IO;
-using System.Linq;
+using System.Reflection;
 using System.Text;
 
 namespace Beavers.Encounter.Common
@@ -42,5 +42,72 @@ namespace Beavers.Encounter.Common
             return SecurityHelper.HashIt(input, algorithm);
         }
         #endregion Security
+
+        #region Exception
+        
+        public static string Expand(this Exception e)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            Exception scanned_exeption = e;
+            Exception ex = new Exception(e.Message, e.InnerException);
+
+            while (scanned_exeption != null)
+            {
+                try
+                {
+                    // Для удобочитаемоси выделяем тип исключения
+                    stringBuilder.Append("---------- ");
+                    stringBuilder.Append(scanned_exeption.GetType().ToString());
+                    stringBuilder.AppendLine(" ----------");
+
+                    PropertyInfo[] props = scanned_exeption.GetType().GetProperties(BindingFlags.Instance | BindingFlags.Public | BindingFlags.FlattenHierarchy);
+                    foreach (PropertyInfo pi in props)
+                    {
+                        // Выводим значения не только строковых полей, но и знечения других полезных свойств-значений.
+                        if (pi.PropertyType.IsAssignableFrom(typeof(string)) || pi.PropertyType.IsValueType)
+                        {
+                            try
+                            {
+                                object value = pi.GetValue(scanned_exeption, null).ToString();
+                                stringBuilder.AppendLine(String.Format("{0}={1}", pi.Name, (value == null ? "" : value.ToString())));
+                            }
+                            catch
+                            {
+                            }
+                        }
+                    }
+                    if (scanned_exeption.Data != null && scanned_exeption.Data.Count > 0)
+                    {
+                        stringBuilder.AppendLine("Data items:");
+                        foreach (DictionaryEntry dataItem in scanned_exeption.Data)
+                        {
+                            stringBuilder.AppendLine(String.Format("  key: {0}, value: {1}", dataItem.Key, dataItem.Value));
+                        }
+                    }
+                }
+                catch
+                {
+                    stringBuilder.AppendLine(string.Format("********** Исключительная ситуация при обработке исключительной ситуации. *********** {0}", e.GetType().ToString()));
+                }
+                scanned_exeption = scanned_exeption.InnerException;
+
+                // Добавляем разрыв для отделения вложенных исключений друг от друга.
+                stringBuilder.AppendLine();
+            }
+            try
+            {
+                stringBuilder.Append("Метод:" + ex.TargetSite.ReflectedType + "." + ex.TargetSite.Name);
+            }
+            catch
+            {
+                // Здесь иногда водникает исключение - просто глушим его.
+            }
+
+            stringBuilder.AppendLine("Пользователь: " + System.Threading.Thread.CurrentPrincipal.Identity.Name);
+
+            return stringBuilder.ToString(); 
+        }
+
+        #endregion Exception
     }
 }
--- app/Beavers.Encounter.Web/Views/Shared/Rescues/Default.aspx
+++ app/Beavers.Encounter.Web/Views/Shared/Rescues/Default.aspx
@@ -1,15 +1,25 @@
 <%@ Page Title="Games" Language="C#" MasterPageFile="~/Views/Shared/Site.Master" AutoEventWireup="true" 
     Inherits="System.Web.Mvc.ViewPage" %>
+<%@ Import Namespace="log4net"%>
 <%@ Import Namespace="System.Security"%>
+<%@ Import Namespace="Beavers.Encounter.Common"%>
 
 <asp:Content ID="Content1" ContentPlaceHolderID="MainContentPlaceHolder" runat="server">
 
     <h2>Error</h2>
 
 <% if(((HandleErrorInfo)ViewData.Model).Exception != null)
- {%>
+ {
+     ILog log = LogManager.GetLogger("ErrorReporter");
+     log.Error(((HandleErrorInfo)ViewData.Model).Exception.Expand());
+    
+    %>
 
-    <%=((HandleErrorInfo)ViewData.Model).Exception.Message %>
+    <div style="color:Red"><%= ((HandleErrorInfo)ViewData.Model).Exception.Message %></div>
+
+    <%= ((HandleErrorInfo)ViewData.Model).Exception.Expand().Replace("\t", "    ")
+                .Replace("\r\n", "<br />")
+    %>
 
 <%
  } else
