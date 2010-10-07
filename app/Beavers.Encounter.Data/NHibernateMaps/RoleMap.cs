0090b492fe0024a54826f66b21d48c0c075b9042
belov.gr <belov.gr@gmail.com>
2010-10-07 21:24:27 +0000

    chg - update libs - Обновление SharpArch до версии 1.6.0 (то вресии 1.5.0).

---
+++ app/Beavers.Encounter.Data/NHibernateMaps/RoleMap.cs
@@ -0,0 +1,19 @@
+﻿using FluentNHibernate.Automapping;
+using FluentNHibernate.Automapping.Alterations;
+
+using Beavers.Encounter.Core;
+
+namespace Beavers.Encounter.Data.NHibernateMaps
+{
+    public class RoleMap : IAutoMappingOverride<Role>
+    {
+        public void Override(AutoMapping<Role> mapping)
+        {
+            mapping.IgnoreProperty(x => x.IsAdministrator);
+            mapping.IgnoreProperty(x => x.IsAuthor);
+            mapping.IgnoreProperty(x => x.IsGuest);
+            mapping.IgnoreProperty(x => x.IsPlayer);
+            mapping.IgnoreProperty(x => x.IsTeamLeader);
+        }
+    }
+}
